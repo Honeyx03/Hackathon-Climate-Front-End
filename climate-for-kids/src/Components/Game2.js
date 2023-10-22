0c6ae6845ab938aa0a6a42cc98e1c11b2642387a
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-10-22 15:36:17 -0400

    updated the Game2 component

--- climate-for-kids/src/Components/Game2.js
+++ climate-for-kids/src/Components/Game2.js
@@ -1,9 +1,13 @@
-export default function Articles() {
+import React, { useEffect } from 'react';
+
+export default function Game2() {
+    useEffect(() => {
+        window.location.href = 'https://frantz-sebastien.github.io/ocean-invaders/';
+    }, []); 
+
     return (
-        <div className="articles">
-            <h1>
-            https://frantz-sebastien.github.io/ocean-invaders/
-            </h1>
+        <div className="game2">
+            <h1>Redirecting...</h1>
         </div>
-    )
+    );
 }
