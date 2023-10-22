65c71ddd3e3ac8d8ea67d338b5679efd9a6d919c
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-10-22 15:23:44 -0400

    Finished About Page, missing styling

--- climate-for-kids/src/App.js
+++ climate-for-kids/src/App.js
@@ -7,6 +7,7 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Articles from "./Components/Game2";
 import Game1 from "./Components/Game1";
+import Game2 from "./Components/Game2"
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/game1" element={<Game1 />} />
+            <Route path="/game2" element={<Game2 />} />
+            <Route path="/about" element={<About />} /> 
           </Routes>
         </div>  
       </Router>
