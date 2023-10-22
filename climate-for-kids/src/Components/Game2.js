943a074f2a7c0e93a4f2b2481f01e521bdfea90c
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-10-22 14:00:33 -0400

    pushing gitHub link to Main Repo

--- climate-for-kids/src/Components/Game2.js
+++ climate-for-kids/src/Components/Game2.js
@@ -2,7 +2,7 @@ export default function Articles() {
     return (
         <div className="articles">
             <h1>
-                This is the articles page
+            https://frantz-sebastien.github.io/ocean-invaders/
             </h1>
         </div>
     )
